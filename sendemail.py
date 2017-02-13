e309686591a9bf836a77322966261dc309c33789
Damion E. Lowers <damion.lowers@gmail.com>
2017-02-12 23:35:09 -0500

    first commit

---
+++ sendemail.py
@@ -0,0 +1,26 @@
+from flask import Flask,request,redirect,abort,render_template
+from flask_script import Manager
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+manager = Manager(app)
+bootstrap = Bootstrap(app)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
+@app.route('/')
+def index():
+	return render_template('index.html');
+
+
+if __name__ == '__main__':
+    manager.run()
